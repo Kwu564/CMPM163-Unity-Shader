f591ae3596be289b6933dff0ce233232fc98d5fd
Kwu564 <tnkboy4@gmail.com>
2018-03-24 18:31:00 -0700

    Add Latest version of the research lab project

---
+++ Lab/Assets/crt/scripts/Lifetime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour {
+
+    public int seconds;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Death());
+	}
+    
+    IEnumerator Death() {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+}
